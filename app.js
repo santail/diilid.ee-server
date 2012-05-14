227d8b7319c0f554923b690642014662692a2184
Nikolai Muhhin <nikolai.muhhin@tieto.com>
2012-05-14 11:53:50 +0300

    Add parsing rules for separate sites

--- app.js
+++ app.js
@@ -61,20 +61,20 @@ app.get('/deals', function (req, res) {
                         site: $site
                     };
 
-                    console.log('waiting to request', deal.href)
+                    console.log('waiting to pakkumised.ee source request', deal.href)
 
                     request({
                         uri: deal.href
                     }, function (err, response, body) {
                         counter--;
 
-                        console.log('counting: ', counter);
+                        console.log('counting pakkumised.ee link: ', counter);
 
                         if (!(err || response.statusCode !== 200) && body) {
                             parsePage(body, function($) {
                                 deal.origin = $('iframe.offerpage_content').attr('src');
 
-                                console.log(deal.origin)
+                                console.log('pakkumised.ee deal origin link', deal.origin)
 
                                 if (deal.origin) {
                                     if (!result.items[$site]) {
@@ -84,20 +84,22 @@ app.get('/deals', function (req, res) {
                                     result.items[$site].push(deal);
                                 }
 
+                                console.log('parsing pakkumised.ee link finished successfully', deal.href)
                                 callback()
                             });
                         }
                         else {
-                            console.log('Error: ', err, deal.href)
+                            console.log('parsing pakkumised.ee link finished with error', deal.href, err)
                             callback()
                         }
                     });
 
                 }, function(err) {
                     if (err) {
-                        console.log('error reading pakkumised.ee original links')
+                        console.log('error reading pakkumised.ee original links', err)
                     }
                     if (counter === 0) {
+                        console.log('parsing pakkumised.ee finished successfully')
                         callback(null, 'one');
                     }
                 })
@@ -115,7 +117,7 @@ app.get('/deals', function (req, res) {
 
                     async.forEachSeries(result.items[$site], function(deal, callback) {
 
-                        console.log('waiting to request', deal.origin)
+                        console.log('waiting request to original deal', deal.origin)
 
                         request({
                             uri: deal.origin,
@@ -164,40 +166,71 @@ app.get('/deals', function (req, res) {
                                             map: $('#container .c-info .inner .form-item .Gmap').attr('src')
                                         }
                                     }
-
                                     if ($site === 'www.ostulaine.ee') {
                                         deal.title = {
                                             full: $('#body_left .main_deal_title').text(),
                                             short: ''
                                         }
                                     }
-
                                     if ($site === 'www.seiklused.ee') {
                                         deal.title = {
                                             full: $('#strip > b').text(),
                                             short: $('#separator403 > b').text()
                                         }
                                     }
-
                                     if ($site === 'www.headiil.ee') {
                                         deal.title = {
                                             full: $('#body_left > h1').text(),
                                             short: ''
                                         }
                                     }
-
                                     if ($site === 'www.chilli.ee') {
                                         deal.title = {
                                             full: $('#buy_box > h1 > a').text(),
                                             short: ''
                                         }
                                     }
-
                                     if ($site === 'www.ediilid.ee') {
                                         $('.leftSide .box1 .mainOfferTitleArea > p > span').remove()
 
                                         deal.title = {
                                             full: $('.leftSide .box1 .mainOfferTitleArea > p').text(),
+                                            short: ''
+                                        }
+                                    }
+                                    if ($site === 'www.ostulaine.ee') {
+                                        deal.title = {
+                                            full: '',
+                                            short: ''
+                                        }
+                                    }
+                                    if ($site === 'www.niihea.ee') {
+                                        deal.title = {
+                                            full: '',
+                                            short: ''
+                                        }
+                                    }
+                                    if ($site === 'www.zizu.ee') {
+                                        deal.title = {
+                                            full: '',
+                                            short: ''
+                                        }
+                                    }
+                                    if ($site === 'www.cherry.ee') {
+                                        deal.title = {
+                                            full: '',
+                                            short: ''
+                                        }
+                                    }
+                                    if ($site === 'www.hotelliveeb.ee') {
+                                        deal.title = {
+                                            full: '',
+                                            short: ''
+                                        }
+                                    }
+                                    if ($site === 'www.minuvalik.ee') {
+                                        deal.title = {
+                                            full: '',
                                             short: ''
                                         }
                                     }
@@ -257,6 +290,8 @@ var parsePage = function (body, parser) {
         else {
             console.log('Error: ', err);
         }
+
+//        window.close()
     });
 };
 
