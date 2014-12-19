653a6a7e749c71b082dbf64201aebab25c391bc4
nikolai muhhin <test@test.ee>
2014-12-19 13:34:22 +0000

    NewRelic config added

---
+++ newrelic.js
@@ -0,0 +1,24 @@
+/**
+ * New Relic agent configuration.
+ *
+ * See lib/config.defaults.js in the agent distribution for a more complete
+ * description of configuration variables and their potential values.
+ */
+exports.config = {
+  /**
+   * Array of application names.
+   */
+  app_name : ['diilid.ee_server'],
+  /**
+   * Your New Relic license key.
+   */
+  license_key : '3d1b36c0422fbf210445aa0821309324b0e2fe38',
+  logging : {
+    /**
+     * Level at which to log. 'trace' is most useful to New Relic when diagnosing
+     * issues with the agent, 'info' and higher will impose the least overhead on
+     * production applications.
+     */
+    level : 'trace'
+  }
+};
