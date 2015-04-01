f9145781ef2add250ba55386367bc79a245bed01
nikolai muhhin <test@test.ee>
2015-04-01 14:50:29 +0000

    Code cleanup

--- apps/models/www.minuvalik.ee.js
+++ apps/models/www.minuvalik.ee.js
@@ -1,73 +1,71 @@
 'use strict';
 
 var util = require('util'),
-    urlParser = require("url"),
-    AbstractParser = require("./abstractParser");
+  urlParser = require("url"),
+  AbstractParser = require("./abstractParser");
 
 function MinuvalikParser() {
-    AbstractParser.call(this);
+  AbstractParser.call(this);
 
-    var that = this;
+  var that = this;
 
-    this.config = {
-        'site': 'www.minuvalik.ee',
-        'index': {
-            'rus': 'https://www.minuvalik.ee/ru/?c=all',
-            'est': 'https://www.minuvalik.ee/?c=all'
+  this.config = {
+    'site': 'www.minuvalik.ee',
+    'reactivate': true,
+    'index': {
+      'rus': 'https://www.minuvalik.ee/ru/?c=all',
+      'est': 'https://www.minuvalik.ee/?c=all'
+    },
+    'list': function ($) {
+      return $('div.deals li > a').map(function () {
+        return $(this).attr('href');
+      });
+    },
+    'templates': {
+      'title': function ($) {
+        return $('.deal_rules_td > h1.title_deal').text();
+      },
+      'pictures': function ($, language) {
+        return $('.deal_rules_td .dd_video_photo > a').map(function () {
+          return that.compileImageUrl(language, $(this).attr('href'));
+        });
+      },
+      'description': {
+        'intro': function ($) {
+          return $('.deal_rules_td .dd_lead').html();
         },
-        'list': function ($) {
-            return $('div.deals li > a').map(function () {
-                return $(this).attr('href');
-            });
+        'short': function ($) {
+          return $('.deal_rules_td .dd_descr').eq(1).html();
         },
-        'templates': {
-            'title': function ($) {
-                return $('.deal_rules_td > h1.title_deal').text();
-            },
-            'pictures': function ($, language) {
-                return $('.deal_rules_td .dd_video_photo > a').map(function () {
-                    return that.compileImageUrl(language, $(this).attr('href'));
-                });
-            },
-            'description': {
-                'intro': function ($) {
-                    return $('.deal_rules_td .dd_lead').html();
-                },
-                'short': function ($) {
-                    return $('.deal_rules_td .dd_descr').eq(1).html();
-                },
-                'long': function ($) {
-                    return $('.deal_rules_td .dd_descr').first().html();
-                }
-            },
-            'price': {
-                'original': function ($) {
-                    return $('.deal_rules_td > div#parent_div div.dd_table_discount_info > span.dd_basic_price').text();
-                },
-                'discount': function ($) {
-                    return $('.deal_rules_td > div#parent_div > div> div.dd_table_price').text();
-                },
-                'save': function ($) {
-                    return $('.deal_rules_td > div#parent_div div.dd_table_discount_info > span.fl_deals_fp_discount_row').text();
-                }
-            }
+        'long': function ($) {
+          return $('.deal_rules_td .dd_descr').first().html();
         }
-    };
+      },
+      'price': {
+        'original': function ($) {
+          return $('.deal_rules_td > div#parent_div div.dd_table_discount_info > span.dd_basic_price').text();
+        },
+        'discount': function ($) {
+          return $('.deal_rules_td > div#parent_div > div> div.dd_table_price').text();
+        },
+        'save': function ($) {
+          return $('.deal_rules_td > div#parent_div div.dd_table_discount_info > span.fl_deals_fp_discount_row').text();
+        }
+      }
+    }
+  };
 }
 
 util.inherits(MinuvalikParser, AbstractParser);
 
 MinuvalikParser.prototype.compileImageUrl = function (language, link) {
-    console.log('Image link', language, link);
-
   var that = this;
   return urlParser.resolve(that.config.index[language], link);
 };
 
 MinuvalikParser.prototype.compileOfferUrl = function (language, link) {
-    var that = this;
-
-    return urlParser.resolve(that.config.index[language], link);
+  var that = this;
+  return urlParser.resolve(that.config.index[language], link);
 };
 
 
