35c02982f278a8055878d1b21020574aacd57eeb
nikolai muhhin <test@test.ee>
2015-04-14 13:19:13 +0000

    Parse default picture in addition to gallery pictures

--- apps/models/www.euronics.ee.js
+++ apps/models/www.euronics.ee.js
@@ -11,6 +11,7 @@ function EuronicsParser() {
 
   this.config = {
     'site': 'www.euronics.ee',
+    'reactivate': true,
     'index': {
       'rus': 'http://www.euronics.ee/products/c/143',
       'est': 'http://www.euronics.ee/tooted/c/143',
@@ -50,9 +51,13 @@ function EuronicsParser() {
         return $('div.oi-section-main-content.clear div.oi-main-article-header > h1').text();
       },
       'pictures': function ($, language) {
+        var pictureUrls = [];
+
         var mainPictureUrl = $('div.oi-section-main-content.clear div.oi-product-media > p.thumb > a').attr('href');
 
-        var pictureUrls = [that.compileImageUrl(language, mainPictureUrl)];
+        if (mainPictureUrl) {
+          pictureUrls.push(that.compileImageUrl(language, mainPictureUrl));
+        }
 
         $('div.oi-section-main-content.clear div.oi-viewport-media > ol > li > a[data-img]').each(function () {
           pictureUrls.push(that.compileImageUrl(language, $(this).attr('href')));
