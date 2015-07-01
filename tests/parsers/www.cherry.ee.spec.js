e7547e91e0e0692754a069d59e6553de79ccfd0b
nikolai muhhin <test@test.ee>
2015-07-01 11:02:31 +0000

    Add unit tests for minuvalik.ee and cherry.ee parsers

---
+++ tests/parsers/www.cherry.ee.spec.js
@@ -0,0 +1,70 @@
+'use strict';
+
+var expect = require('chai').expect,
+  _ = require("underscore")._,
+  fs = require("fs"),
+  CherryParser = require('../../apps/parsers/www.cherry.ee.js');
+
+var urls = {
+  'offer': {
+    'est': 'https://cherry.ee/16-osaline-sushikomplekt',
+    'rus': 'https://cherry.ee/ru/16-osaline-sushikomplekt'
+  }
+};
+
+var parser = new CherryParser();
+
+describe(parser.config.site, function () {
+
+  describe("Layout has not been changed", function () {
+
+    /*
+    it("Parsing live index page should return at least some none empty deals", function (done) {
+      parser.checkIndexLayoutChanged({}, function (res) {
+        console.log(res);
+        expect(res.changed).to.be.false;
+        done();
+      });
+    });
+
+    it("Parsed live deal page should match with ethalon data", function () {
+      _.each(parser.sampleDealUrls, function (url, language) {
+        parser.checkDealLayoutChanged(data, {}, function (res) {
+          expect(res).to.not.be.empty;
+        });
+      });
+    });
+
+    it("Parsing ethalon index page should return exact the same data as ethalon index page", function (done) {
+      fs.readFile((__dirname + '/../sources/' + parser.site + '-' + language + '-index.html', 'utf8') + '', function (err, data) {
+        if (!err) {
+          parser.fetchIndexList(data, {}, function (res) {
+            expect(res).to.not.be.empty;
+            done();
+          });
+        }
+      });
+    });
+    */
+
+    _.each(_.keys(parser.config.index), function (language) {
+      it("Parsing offer's page for " + language + " should return proper data", function (done) {
+        var offerDataFile = __dirname + '/' + parser.config.site + '.data.' + language + '.json',
+          offerBodyFile = __dirname + '/' + parser.config.site + '.offer.' + language + '.html';
+
+          var data = JSON.parse(fs.readFileSync(offerDataFile, 'utf8'));
+
+          fs.readFile(offerBodyFile, 'utf8', function (err, body) {
+            if (!err) {
+              parser.parseOffer(body, language, function (err, res) {
+                expect(err).to.be.empty;
+                expect(res).to.not.be.empty;
+                expect(data).to.deep.equal(res);
+                done();
+              });
+            }
+          });
+      });
+    });
+  });
+});
