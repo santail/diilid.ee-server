3c3be71d97c19b0072a0dcf2d6ebd349e88bc77f
nikolai muhhin <test@test.ee>
2015-07-22 15:42:32 +0000

    Code cleanup

--- apps/parsers/pakkumised.ee.js
+++ apps/parsers/pakkumised.ee.js
@@ -1,52 +1,48 @@
 'use strict';
 
 var util = require('util'),
-    _ = require("underscore")._,
-    AbstractParser = require("./abstractParser"),
+  _ = require("underscore")._,
+  AbstractParser = require("./abstractParser"),
   LOG = require("../services/Logger");
 
 function PakkumisedParser() {
-    AbstractParser.call(this);
+  AbstractParser.call(this);
 
-    this.config = {
-        'index': {
-            'est': 'http://pakkumised.ee/acts/offers/js_load.php?act=offers.js_load&category_id=0&page='
-        },
-        'site' : 'www.pakkumised.ee',
-        'list': function ($) {
+  this.config = {
+    'index': {
+      'est': 'http://pakkumised.ee/acts/offers/js_load.php?act=offers.js_load&category_id=0&page='
+    },
+    'site': 'www.pakkumised.ee',
+    'list': function ($) {
 
+    },
+    'templates': {
+      'title': function ($) {
+        return $('div#deal-info h2 > a').text();
+      },
+      'pictures': function ($) {
+        return $('#deal-image div.carousel-inner div.item a > img').map(function () {
+          return $(this).attr('src');
+        });
+      },
+      'description': {
+        'short': function ($) {
+          // TODO needs to be cleared out
+          return $('#themainthing div.offer-details div.offer-contents div.left-side div#long_text_container').html();
         },
-        'templates': {
-            'title': function ($) {
-                return $('div#deal-info h2 > a').text();
-            },
-            'pictures': function ($) {
-                return $('#deal-image div.carousel-inner div.item a > img').map(function () {
-                    return $(this).attr('src');
-                });
-            },
-            'description': {
-                'short': function ($) {
-                    // TODO needs to be cleared out
-                    return $('#themainthing div.offer-details div.offer-contents div.left-side div#long_text_container').html();
-                },
-                'long': function ($) {
-                    // TODO needs to be cleared out
-                    return $('#themainthing div.offer-details div.offer-contents div.left-side').html();
-                }
-            }
+        'long': function ($) {
+          // TODO needs to be cleared out
+          return $('#themainthing div.offer-details div.offer-contents div.left-side').html();
         }
-    };
+      }
+    }
+  };
 }
 
 util.inherits(PakkumisedParser, AbstractParser);
 
 AbstractParser.prototype.getOfferLinks = function (data, language) {
   var that = this;
-
-    LOG.debug({
-      'Offers': data
-    });
 
   return _.map(data, function (offer) {
     return that.compileOfferUrl(language, offer.url);
@@ -55,10 +51,6 @@ AbstractParser.prototype.getOfferLinks = function (data, language) {
 
 PakkumisedParser.prototype.parseResponseBody = function (data) {
   try {
-    LOG.debug({
-      'JSON': data
-    });
-
     return JSON.parse(data);
   }
   catch (ex) {
