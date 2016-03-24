7c0f5148b3ec8b92375a0401ade21e7c47b2f12f
nikolai muhhin <test@test.ee>
2016-03-24 21:20:36 +0000

    Update discount price template

--- apps/parsers/www.euronics.outlet.ee.js
+++ apps/parsers/www.euronics.outlet.ee.js
@@ -2,7 +2,8 @@
 
 var util = require('util'),
   AbstractEuronicsParser = require("./AbstractEuronicsParser"),
-  _ = require("underscore")._;
+  _ = require("underscore")._,
+  utils = require("../services/Utils");
 
 function EuronicsOutletParser() {
   AbstractEuronicsParser.call(this);
@@ -17,6 +18,10 @@ function EuronicsOutletParser() {
   };
 
   this.config = _.extend(this.config, config);
+  
+  this.config.templats.price = function ($) {
+    return utils.unleakString($('div.oi-section-main-content.clear div.oi-product-description > div.oi-bottom > ul > li > p > span').text());
+  };
 }
 
 util.inherits(EuronicsOutletParser, AbstractEuronicsParser);
